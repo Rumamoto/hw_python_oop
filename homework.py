4ad6464235b2305a96051f9ffa46f000f31f5dea
Дмитрий Шкурин <dmitriyevroman205@mail.ru>
2022-09-20 18:23:18 +0300

    Сделал задания по 2 ревью

--- homework.py
+++ homework.py
@@ -1,53 +1,43 @@
-from typing import List, Union, Dict, Type
+from dataclasses import asdict, dataclass
+from typing import ClassVar, Dict, List, Type, Union
 
 
+@dataclass
 class InfoMessage:
     """Информационное сообщение о тренировке."""
 
-    def __init__(self, training_type: str,
-                 duration: float,
-                 distance: float,
-                 speed: float,
-                 calories: float) -> None:
+    training_type: int = " "
+    duration: float = 0.0
+    distance: float = 0.0
+    speed: float = 0.0
+    calories: float = 0.0
 
-        self.training_type = training_type
-        self.duration = duration
-        self.distance = distance
-        self.speed = speed
-        self.calories = calories
-
-    MESS_TRAINING = ("Тип тренировки: {training_type}; "
-                     "Длительность: {duration:.3f} ч.; "
-                     "Дистанция: {distance:.3f} км; "
-                     "Ср. скорость: {speed:.3f} км/ч; "
-                     "Потрачено ккал: {calories:.3f}.")
+    MESS_TRAINING: ClassVar[str] = ("Тип тренировки: {training_type}; "
+                                    "Длительность: {duration:.3f} ч.; "
+                                    "Дистанция: {distance:.3f} км; "
+                                    "Ср. скорость: {speed:.3f} км/ч; "
+                                    "Потрачено ккал: {calories:.3f}.")
 
     def get_message(self):
-        return self.MESS_TRAINING.format(
-            training_type=self.training_type,
-            duration=self.duration,
-            distance=self.distance,
-            speed=self.speed,
-            calories=self.calories
-        )
+        return self.MESS_TRAINING.format(**asdict(self))
 
 
 class Training:
-    """Базовый класс тренировки."""
     LEN_STEP: float = 0.65
     M_IN_KM: int = 1000
     MIN_IN_HOURS: int = 60
+    """Базовый класс тренировки."""
 
     def __init__(self,
                  action: int,
                  duration: float,
                  weight: float,
                  ) -> None:
-        '''
+        """
         action: количество совершённых действий число шагов,беге либо гребков
         duration: длительность тренировки
         weight: вес спортсмена
-        '''
+        """
         self.action = action
         self.duration = duration
         self.weight = weight
@@ -55,7 +45,6 @@ class Training:
     def get_distance(self) -> float:
         """Получить дистанцию в км."""
         return self.action * self.LEN_STEP / self.M_IN_KM
-    '''Расчёт дистанции, которую пользователь преодолел за тренировку.'''
 
     def get_mean_speed(self) -> float:
         """Получить среднюю скорость движения."""
@@ -77,9 +66,9 @@ class Training:
 
 
 class Running(Training):
-    """Тренировка: бег."""
     RUN_COEFF_CALORIE_1 = 18
     RUN_COEFF_CALORIE_2 = 20
+    """Тренировка: бег."""
 
     def get_spent_calories(self) -> float:
         return ((self.RUN_COEFF_CALORIE_1 * self.get_mean_speed()
@@ -90,9 +79,9 @@ class Running(Training):
 
 
 class SportsWalking(Training):
-    """Тренировка: спортивная ходьба."""
     WLK_COEFF_CAL_1 = 0.035
     WLK_COEFF_CAL_2 = 0.029
+    """Тренировка: спортивная ходьба."""
 
     def __init__(
             self,
@@ -112,8 +101,10 @@ class SportsWalking(Training):
 
 
 class Swimming(Training):
-    """Тренировка: плавание."""
+    SWM_COEFF_CALORIE_1 = 1.1
+    SWM_COEFF_CALORIE_2 = 2
     LEN_STEP = 1.38  # переопределение длины гребка, вместо длины шага
+    """Тренировка: плавание."""
 
     def __init__(
             self,
@@ -126,21 +117,15 @@ class Swimming(Training):
         self.length_pool = length_pool
         self.count_pool = count_pool
 
-    def get_distance(self):
-        """Получить дистанцию в км."""
-        return self.action * self.LEN_STEP / self.M_IN_KM
-
     def get_spent_calories(self) -> float:
-        '''Формула для расчёта израсходованных калорий.'''
-        SWM_COEFF_CALORIE_1 = 1.1
-        SWM_COEFF_CALORIE_2 = 2
+        """Формула для расчёта израсходованных калорий."""
         return (
-            (self.get_mean_speed() + SWM_COEFF_CALORIE_1)
-            * SWM_COEFF_CALORIE_2 * self.weight
+            (self.get_mean_speed() + self.SWM_COEFF_CALORIE_1)
+            * self.SWM_COEFF_CALORIE_2 * self.weight
         )
 
     def get_mean_speed(self) -> float:
-        '''Формула расчёта средней скорости при плавании.'''
+        """Формула расчёта средней скорости при плавании."""
         return (
             self.length_pool * self.count_pool / self.M_IN_KM
             / self.duration
@@ -149,12 +134,12 @@ class Swimming(Training):
 
 def read_package(workout_type: str, data: List[Union[int, float]]) -> Training:
     """Прочитать данные полученные от датчиков."""
-    training_dict: Dict[str, Type[Training]] = {"SWM": Swimming,
-                                                "RUN": Running,
-                                                "WLK": SportsWalking}
-    if workout_type not in training_dict:
-        raise ValueError(f'Тип тренировки {workout_type} не определен.')
-    return training_dict[workout_type](*data)
+    training: Dict[str, Type[Training]] = {"SWM": Swimming,
+                                           "RUN": Running,
+                                           "WLK": SportsWalking}
+    if workout_type not in training:
+        raise ValueError(f"Тип тренировки {workout_type} не определен.")
+    return training[workout_type](*data)
 
 
 def main(training: Training):
@@ -162,11 +147,11 @@ def main(training: Training):
     print(training.show_training_info().get_message())
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     packages = [
-        ('SWM', [720, 1, 80, 25, 40]),
-        ('RUN', [15000, 1, 75]),
-        ('WLK', [9000, 1, 75, 180]),
+        ("SWM", [720, 1, 80, 25, 40]),
+        ("RUN", [15000, 1, 75]),
+        ("WLK", [9000, 1, 75, 180]),
     ]
 
     for workout_type, data in packages:
